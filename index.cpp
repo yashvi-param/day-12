8bbac26feb094cb27350796de2ef00cde45da302
yashvi-param <yashvirwn123@gmail.com>
2025-03-11 10:53:16 +0530

    Add files via upload
    complete day-12

---
+++ index.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //int n = 5;
+
+// for(int i=5; i>=1; i--)  
+// {
+//     for(int j=i; j<=5; j++)
+//     {
+//             cout << j << " ";
+//     }
+//     cout << endl;
+// }
+
+
+
+
+        // int row;
+        // int col;
+
+        //          cout << "row: ";
+        //          cin >> row;
+
+        //          cout << "col: ";
+        //         cin >> col;
+
+//     for (int i=0; i<row; i++)
+//     {
+//         for (int j=0; j<col; j++)
+//         {
+//             cout << "*" << " ";
+//         }
+//         cout << endl;
+//     }
+
+
+//     for (int i=1; i<row; i++)
+//     {
+//         for (int j=col; j>0; j--)
+//         {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+
+
+        // int n = 5;
+
+        // for (int i=1; i<=5; i++)
+        // {
+        //     for (int j=1; j<=i; j++)
+        //     {
+        //         cout << "*" << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+
+
+
+
+
+
+
+        // int n = 5;
+
+        // for (int i=1; i<=5; i++)
+        // {
+        //     for (int j=1; j<=i; j++)
+        //     {
+        //         cout << j << " " ;
+        //     }
+        //     cout << endl;
+        // }
+
+
+        
+        int n = 5;
+
+        for (int i=1; i<=5; i++)
+        {
+            for (int j=1; j<=i; j++)
+            {
+                cout << i << " " ;
+            }
+            cout << endl;
+        }
+
+
+        // int n = 5;
+
+
+        // for (int i=1; i<=5; i++)
+        // {
+        //     for(int j=1; j<=5; j++)
+        //     {
+        //         cout << j << " ";
+        //     }
+        //     cout << endl;
+        // }
+
+        return 0;
+ }
+
+
+
+
+
+
+
+ 
+     
+
+
+
+
+   
+
+
